7d39c20ce3a7983c9e20609d362b79b5c69dc7fb
GHV 23 <GHV 23@GHV>
2024-11-05 15:14:32 -0500

    5-11

---
+++ src/main/java/edu/gamma/proyecto/gamma/ProyectoGamma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.gamma.proyecto.gamma;
+
+/**
+ *
+ * @author isabel
+ */
+public class ProyectoGamma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
