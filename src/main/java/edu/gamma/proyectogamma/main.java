984a465e4e9f8cfbda0199367ece1c5633a85cc8
Isabel P <>
2024-11-04 23:09:39 -0500

    Cambios nuevos

--- src/main/java/edu/gamma/proyectogamma/main.java
+++ src/main/java/edu/gamma/proyectogamma/main.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main.model;
+package edu.gamma.proyectogamma;
 
 /**
  *
  * @author Asus
  */
-public class Alumno {
+public class main {
     
 }
